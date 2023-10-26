ce7eff97b60bf23dee40f9de4465e7dcb2acb1db
isabelmelgui <143104109+isabelmelgui@users.noreply.github.com>
2023-10-26 10:42:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -85,16 +85,12 @@ def text_to_speech(text, tld):
 
 #display_output_text = st.checkbox("Verifica el texto")
 
-if st.button("Escuchar características"):
-    result, output_text = text_to_speech(response, tld)
+if st.button("Escuchar"):
+    result = text_to_speech(response, tld)
     audio_file = open(f"temp/{result}.mp3", "rb")
     audio_bytes = audio_file.read()
     st.markdown(f"## Tú audio:")
     st.audio(audio_bytes, format="audio/mp3", start_time=0)
-
-    #if display_output_text:
-    st.markdown(f"## Texto en audio:")
-    st.write(f" {output_text}")
 
 
 def remove_files(n):
